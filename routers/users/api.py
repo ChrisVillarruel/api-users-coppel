a8e37a43e46eb60438518eaf6808a4f5d4f60457
ChrisGil <givc980909@gmail.com>
2023-01-04 00:18:02 -0600

    Se crea servicio de login

--- routers/users/api.py
+++ routers/users/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
-from config.backends import set_password
+from config.backends import set_password, CreateJWTToken, get_password
 from config.db import client as db
 
 router = APIRouter()
@@ -26,3 +26,26 @@ async def register_user(user: RegisterUser):
         user.password = set_password(user.password)
         my_collection.insert_one(user.dict())
         return {"status": "Usuario creado exitosamente!"}
+
+
+class Login(BaseModel):
+    email: str
+    password: str
+
+
+@router.post("/login/")
+async def login_user(login: Login):
+    my_db = db["miapp"]
+    my_collection = my_db["users"]
+    user = my_collection.find_one({"email": login.email})
+
+    if not user:
+        raise HTTPException(detail="Ususario no registrado", status_code=400)
+    elif not get_password(user.get("password"), login.password):
+        raise HTTPException(detail="Dirección de correo electronico y/o contraseña no valido", status_code=400)
+    else:
+        token = CreateJWTToken(user_id=str(user.get("_id"))).create_jwt()
+        query = {"_id": user.get("_id")}
+        newvalues = {"$set": {'token': token}}
+        my_collection.update_one(query, newvalues)
+        return {"status": f"Bienvenido {user.get('name')}!!!", "token": token}
