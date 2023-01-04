bfc0412a7014a9bf3b6c626a73414d853616dd11
ChrisGil <givc980909@gmail.com>
2023-01-03 23:52:29 -0600

    Se importa las constantes globales con los accesos a la base de datos de MongoDB

--- config/db.py
+++ config/db.py
@@ -1,10 +1,6 @@
 import pymongo
-
-USERNAME = "nico"
-PASSWORD = "4busG15IKu9aqj66"
-DATABASE = "miapp"
+from constans import USERNAME, PASSWORD, DATABASE
 
 conn_str = f"mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.h3y8s8p.mongodb.net/{DATABASE}?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
-client.server_info()
+client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=90000)
