81800d90da2a80f704c1b16cf7ee1aec6345f2a8
ChrisGil <givc980909@gmail.com>
2023-01-04 00:19:08 -0600

    Se agrega servicio para regresar la información del usuario utilizando un token de autenticación

--- routers/users/api.py
+++ routers/users/api.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from bson import ObjectId
+from fastapi import APIRouter, HTTPException, Header
 from pydantic import BaseModel
 
-from config.backends import set_password, CreateJWTToken, get_password
+from config.backends import set_password, CreateJWTToken, get_password, DecodeJWTToken
 from config.db import client as db
 
 router = APIRouter()
@@ -49,3 +50,44 @@ async def login_user(login: Login):
         newvalues = {"$set": {'token': token}}
         my_collection.update_one(query, newvalues)
         return {"status": f"Bienvenido {user.get('name')}!!!", "token": token}
+
+
+async def verify_token(x_token: str):
+    try:
+        prefix = "Bearer"
+        autorization_token = x_token.split(" ")
+        prefix_token = autorization_token[0]
+        token = autorization_token[1]
+
+        if len(autorization_token) > 2:
+            raise HTTPException(detail="Prefijo de token no valido", status_code=400)
+        if prefix != prefix_token:
+            raise HTTPException(detail="Prefijo de token no valido", status_code=400)
+
+        jwt_decode = DecodeJWTToken(token).decode_jwt()
+        return jwt_decode.get("iss")
+
+    except ValueError as e:
+        raise HTTPException(detail=str(e), status_code=400)
+    except Exception as e:
+        raise HTTPException(detail=f"Error al autenticar su token", status_code=400)
+
+
+async def user_data(user: dict):
+    return {
+        "id": str(user.get("_id")),
+        "name": user.get("name"),
+        "age": user.get("age"),
+        "token": user.get("token"),
+    }
+
+
+@router.get("/user/")
+async def user_info(x_token: str = Header()):
+    user_id = await verify_token(x_token)
+    my_db = db["miapp"]
+    my_collection = my_db["users"]
+    data = my_collection.find_one({'_id': ObjectId(user_id)})
+    if not data:
+        raise HTTPException(detail="Usuario no encontrado", status_code=400)
+    return await user_data(data)
