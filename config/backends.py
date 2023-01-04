89581ec7f5cd75d0f124c0c142600bc696dd0eb8
ChrisGil <givc980909@gmail.com>
2023-01-04 00:01:01 -0600

    Se crea funcionalidad para generar un token de auotrización y decodificar un token de autorización

--- config/backends.py
+++ config/backends.py
@@ -1,5 +1,10 @@
+import datetime as dt
+from typing import ClassVar
+
+import jwt
+
 from cryptography.fernet import Fernet
-from constans import SYMMETRIC_JEY
+from constans import SYMMETRIC_JEY, DEFAULT_EXPIRATION_MINUTES, JWT_KEY
 
 
 def set_password(user_password: str) -> str:
@@ -14,3 +19,51 @@ def get_password(hash_user_password: str, user_password) -> bool:
     if decrypt_password.decode() == user_password:
         return True
     return False
+
+
+class CreateJWTToken:
+    _DEFAULT_EXPIRATION_MINUTES: ClassVar[int] = DEFAULT_EXPIRATION_MINUTES
+
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+
+    @staticmethod
+    def _unix_time(date_time: dt.datetime):
+        return dt.datetime.timestamp(date_time)
+
+    @property
+    def payload(self) -> dict:
+        return {
+            "iss": self.user_id,
+            "exp": self._unix_time(dt.datetime.now() + dt.timedelta(minutes=self._DEFAULT_EXPIRATION_MINUTES)),
+            "iat": self._unix_time(dt.datetime.now())
+        }
+
+    @property
+    def headers(self) -> dict:
+        return {
+            "alg": "HS256",
+            "typ": "JWT"
+        }
+
+    def create_jwt(self) -> str:
+        return jwt.encode(
+            payload=self.payload,
+            headers=self.headers,
+            key=JWT_KEY
+        )
+
+
+class DecodeJWTToken:
+    def __init__(self, jwt_token: str):
+        self.jwt_token = jwt_token
+
+    def decode_jwt(self) -> dict:
+        try:
+            return jwt.decode(jwt=self.jwt_token, key=JWT_KEY, algorithms="HS256")
+        except jwt.ExpiredSignatureError as _:
+            raise ValueError("El token de acceso ya expiro")
+        except jwt.exceptions as e:
+            raise ValueError(str(e))
+        except Exception as e:
+            raise ValueError(str(e))
