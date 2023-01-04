3d1c0427c401775d0d666b6e4f571a7608862268
ChrisGil <givc980909@gmail.com>
2023-01-04 00:12:54 -0600

    Se realiza la prueba de unidad de las clases TestCaseCreateJWTToken y TestCaseDecodeJWTToken

--- testing/test_users.py
+++ testing/test_users.py
@@ -1,5 +1,6 @@
 import unittest
-from config.backends import set_password, get_password
+
+from config.backends import set_password, get_password, DecodeJWTToken, CreateJWTToken
 
 
 class TestCaseHashPasswordUser(unittest.TestCase):
@@ -19,3 +20,27 @@ class TestCaseHashPasswordUser(unittest.TestCase):
 
         self.assertIsInstance(password, bool)
         self.assertIs(password, True)
+
+
+class TestCaseCreateJWTToken(unittest.TestCase):
+    def setUp(self) -> None:
+        self.token = CreateJWTToken
+
+    def test_create_jwt_token(self):
+        token = self.token(user_id="56122589966210001")
+        self.assertIsInstance(token.payload, dict)
+        self.assertIsInstance(token.payload.get("iss"), str)
+        self.assertIsInstance(token.create_jwt(), str)
+
+
+class TestCaseDecodeJWTToken(unittest.TestCase):
+    def setUp(self) -> None:
+        self.token = DecodeJWTToken
+
+    def test_create_jwt_token(self):
+        token = self.token(
+            jwt_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2M2I1MTgzNzYyNDkwOWZjMjJiNjFjNTciLCJleHAiOjE2NzM2NzY2MDQuNTgxMTE5LCJpYXQiOjE2NzI4MTI2MDQuNTgxMTY2fQ._jPxazT5iiY3Yjwg5zWyWMSoQojbIYGoC5G_QvTl-c0")
+        self.assertIsInstance(token.decode_jwt(), dict)
+
+
+unittest.main()
