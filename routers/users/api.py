53245aca1f0acbefdd98fd365bbd86db6a00861b
ChrisGil <givc980909@gmail.com>
2023-01-05 17:01:35 -0600

    Se agrega modificaciones en
    	1. login_user
    		Se agrega nuevo campo de base datos (is_active)
    		Se implementa funcionalidad publish que se envia a rabbitmq
    	2. verify_token
    		Se agrega modificación de la bandera is_Active si el token del usurio caduco
    	3. RegisterUser
    		Se agrega campo is_active

--- routers/users/api.py
+++ routers/users/api.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 from config.backends import set_password, CreateJWTToken, get_password, DecodeJWTToken
 from config.db import client as db
 
+from producer import publish
+
 router = APIRouter()
 
 
@@ -15,6 +17,7 @@ class RegisterUser(BaseModel):
     password: str
     age: int
     token: str | None = None
+    is_active: bool = False
 
 
 @router.post('/register-user/')
@@ -34,6 +37,17 @@ class Login(BaseModel):
     password: str
 
 
+async def data_user_info(user: dict):
+    """ Información que se publicara en RabbitMQ """
+
+    return {
+        "user_id": str(user.get("_id")),
+        "name": user.get("name"),
+        "is_active": user.get("is_active"),
+        "token": user.get("token"),
+    }
+
+
 @router.post("/login/")
 async def login_user(login: Login):
     my_db = db["miapp"]
@@ -47,8 +61,11 @@ async def login_user(login: Login):
     else:
         token = CreateJWTToken(user_id=str(user.get("_id"))).create_jwt()
         query = {"_id": user.get("_id")}
-        newvalues = {"$set": {'token': token}}
+        newvalues = {"$set": {'token': token, "is_active": True}}
         my_collection.update_one(query, newvalues)
+
+        user = my_collection.find_one({"email": login.email})
+        publish("user_login", await data_user_info(user))
         return {"status": f"Bienvenido {user.get('name')}!!!", "token": token}
 
 
@@ -59,14 +76,20 @@ async def verify_token(x_token: str):
         prefix_token = autorization_token[0]
         token = autorization_token[1]
 
+        my_db = db["miapp"]
+        my_collection = my_db["users"]
+        query = {"token": token}
+
         if len(autorization_token) > 2:
+            newvalues = {"$set": {"is_active": False}}
+            my_collection.update_one(query, newvalues)
             raise HTTPException(detail="Prefijo de token no valido", status_code=400)
         if prefix != prefix_token:
+            newvalues = {"$set": {"is_active": False}}
+            my_collection.update_one(query, newvalues)
             raise HTTPException(detail="Prefijo de token no valido", status_code=400)
-
         jwt_decode = DecodeJWTToken(token).decode_jwt()
         return jwt_decode.get("iss")
-
     except ValueError as e:
         raise HTTPException(detail=str(e), status_code=400)
     except Exception as e:
